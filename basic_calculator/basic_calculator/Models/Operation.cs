3cfa1124823ab72f373dc7ee27d5203fefdd091f
damar <damar@DESKTOP-I8MH6T7>
2023-02-26 01:44:41 -0600

    Add project files.

---
+++ basic_calculator/basic_calculator/Models/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace basic_calculator.Models
+{
+    public class Operation
+    {
+       
+        public string Resultoperation { get; set; }
+
+    }
+}
---
+++ basic_calculator/basic_calculator/Views/PageOperation.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace basic_calculator.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageOperation : ContentPage
+    {
+        public PageOperation()
+        {
+            InitializeComponent();
+        }
+
+        double num1 = 0, num2 =0, total = 0;
+
+        private async void Enviar(double resultado)
+        {
+            var result = new Models.Operation
+            {
+                Resultoperation = resultado.ToString()
+
+            };
+
+            var page = new Views.PageResult();
+            page.BindingContext = result;
+            await Navigation.PushAsync(page);
+
+        }
+
+        private void sumar(double num1, double num2)
+        {
+            total = num1 + num2;
+            Enviar(total);
+        }
+        private void restar (double num1, double num2)
+        {
+            total = num1 - num2;
+            Enviar(total);
+        }
+
+        private void multiplicar(double num1, double num2)
+        {
+            total = num1 * num2;
+            Enviar(total);
+        }
+
+        private void dividir(double num1, double num2)
+        {
+            total = num1 / num2;
+            Enviar(total);
+        }
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+             num1 = Convert.ToDouble(txtNum1.Text);
+             num2 = Convert.ToDouble(txtNum2.Text);
+             sumar(num1, num2);
+
+        }
+
+        private void btnRestar_Clicked(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            restar(num1, num2);
+        }
+
+        private void btnMultiplicar_Clicked(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            multiplicar(num1, num2);
+
+        }
+
+        private void btnDividir_Clicked(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtNum1.Text);
+            num2 = Convert.ToDouble(txtNum2.Text);
+            dividir(num1, num2);
+        }
+    }
+}
